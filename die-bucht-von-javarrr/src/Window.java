3c87ad3faf9e8803ac49667532a23d4d6ac427b4
FabianReitz <reitz@stadtwerk.org>
2020-04-23 17:33:24 +0200

    Added MenuBar

---
+++ die-bucht-von-javarrr/src/Window.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame{
+
+	
+	
+	public Window() {
+		
+		Container contentPane = this.getContentPane();
+		
+		// Killt Prozess, wenn Anwendung beendet wird
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		// Erzeugt Menueleiste
+		JMenuBar mbar = new JMenuBar();
+		
+		// Erzeugt Menues
+		JMenu game = new JMenu("Spiel");
+		
+		// Menuepunkte erzeugen
+		JMenuItem start = new JMenuItem("Neues Spiel");
+		JMenuItem highScores = new JMenuItem("Scoreboard");
+		JMenuItem exit = new JMenuItem("Spiel verlassen");
+		
+		// Spiel verlassen
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		
+		// Unterpunkte einfuegen
+		game.add(start);
+		game.add(highScores);
+		game.add(exit);
+		
+		mbar.add(game);
+		
+		Window frame = new Window();
+		frame.setVisible(true);
+		
+		// Setzt die Größe des Fensters
+		frame.setPreferredSize(new Dimension(512, 512));
+		
+		// MenuBar anzeigen
+		frame.setJMenuBar(mbar);
+		
+		frame.pack();
+		
+		
+
+	}
+	
+	public void addActionListener(ActionListener e) {
+		
+	}
+
+}
