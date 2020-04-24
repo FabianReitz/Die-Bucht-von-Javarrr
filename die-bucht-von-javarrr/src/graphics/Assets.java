b3929ecc706c081d72cbf4214fe76e8a58475477
crom96 <64159828+crom96@users.noreply.github.com>
2020-04-24 17:05:36 +0200

    Nix

--- die-bucht-von-javarrr/src/graphics/Assets.java
+++ die-bucht-von-javarrr/src/graphics/Assets.java
@@ -7,7 +7,7 @@ public class Assets {
 	public static BufferedImage background, player, enemysmall;
 	
 	public static void init() {
-		background = ImageLoader.loadImage("Javarrr_Water_512x512_001.png");
+
 		player = ImageLoader.loadImage("Javarrr_player_ship_001.png");
 	}
 }
