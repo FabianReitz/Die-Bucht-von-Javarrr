e45458911e44cebb238bdf7fcb312bb3fd55f458
NiklasVo <nvogt61@gmail.com>
2020-05-06 10:31:28 +0200

    Gegner Movement

--- die-bucht-von-javarrr/src/states/GameState.java
+++ die-bucht-von-javarrr/src/states/GameState.java
@@ -2,6 +2,7 @@ package states;
 
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import game.Game;
 import graphics.Assets;
@@ -12,22 +13,28 @@ import units.Player;
 public class GameState extends State{
 	private Gegner gegner;
 	private Player player;
+	private Gegner gegner1;
 	private Background background;
+	
 	
 	public GameState(Game game) {
 		super(game);
 		player = new Player(game, 256, 450);
 		background = new Background(game);
 		gegner = new Gegner(game, 256, 200);
-		
+		gegner1 = new Gegner(game, 100, 200);
 		
 	}
+
+		
+	
 	
 	@Override
 	public void update() {
 		player.update();
 		background.update();
 		gegner.update();
+		gegner1.update();
 	}
 
 	@Override
@@ -35,6 +42,7 @@ public class GameState extends State{
 		background.render(graphics);
 		player.render(graphics);
 		gegner.render(graphics);
+		gegner1.render(graphics);
 	}
 
 	
