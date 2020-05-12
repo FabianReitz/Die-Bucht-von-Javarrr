b28c829f937bd7b83bc6fbbb960cf72839050249
FabianReitz <reitz@stadtwerk.org>
2020-05-12 12:41:03 +0200

    Player Klasse kommentiert

--- die-bucht-von-javarrr/src/units/Player.java
+++ die-bucht-von-javarrr/src/units/Player.java
@@ -1,4 +1,5 @@
 package units;
+
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
@@ -7,66 +8,71 @@ import graphics.Animation;
 import graphics.Assets;
 import graphics.Shoot;
 
-public class Player extends Unit{
+public class Player extends Unit {
 
-    private Game game;
-    private int damage, kanonen, maxLeben;
-    
-    //Animation
-    private Animation shoot;
-    
-    //Attacktimer   
-    public long reloadStart;
-    public long shootCooldown = 800;
-    private boolean reloading = false;
+	private Game game;
+	private int damage, kanonen, maxLeben;
 
-    public Player(Game game, float x, float y) {
-        super(x,y);
-        this.game = game;
-        damage = 1;
-        kanonen = 1;
-        maxLeben = 100;
-        
-        //Animation
-    }
-    
+	// Animation
+	private Animation shoot;
 
-    public void shoot() {
-        if (game.getKeyManager().statusTasten.contains(KeyEvent.VK_SPACE) && !reloading) {
-            System.out.println("FIRE");
-            reloading = true;
-            reloadStart = System.currentTimeMillis();
-        }
-        if (reloading && ((System.currentTimeMillis() - reloadStart) >= shootCooldown)) {
-            reloading = false;
-        }
-    }
+	// Schusscooldown
+	public long reloadStart;								// Zeitpunkt des letzten Schusses
+	public long shootCooldown = 800;						// Zeit in ms, die vergehen muss, bis der Spieler wieder schießen kann.
+	private boolean reloading = false;						// Boolean, die auf true steht, wenn der Spieler gerade nachlädt.
 
-    private void getInput() {
-        xMove = 0;
+	public Player(Game game, float x, float y) {
+		super(x, y);
+		this.game = game;
+		damage = 1;
+		kanonen = 1;
+		maxLeben = 100;
+	}
 
-        if(game.getKeyManager().statusTasten.contains(KeyEvent.VK_A) && x > 1) {
-            xMove = -movespeed;
+	
+	// Methode zum Steuern der Schüsse des Spielers:
+	public void shoot() {
+		
+		// Wird die Leertaste gedrückt und der Spieler muss nicht nachladen...
+		if (game.getKeyManager().statusTasten.contains(KeyEvent.VK_SPACE) && !reloading) {
+			System.out.println("FIRE");						// ... gib einen Schuss ab.
+			reloading = true;								// ... setze Nachladen auf true.
+			reloadStart = System.currentTimeMillis();		// ... bestimme den Zeitpunkt des Nachladens.
+		}
+		
+		// Muss der Spieler nachladen und es ist eine Zeit von mindestens "shootCooldown" vergangen...
+		if (reloading && ((System.currentTimeMillis() - reloadStart) >= shootCooldown)) {
+			reloading = false;								// ... setze Nachladen auf false.
+		}
+	}
 
-        }
+	// Methode zum Bewegen des Spielers:
+	private void getInput() {
+		xMove = 0;											// Setze die relative Position auf 0.
 
-        if(game.getKeyManager().statusTasten.contains(KeyEvent.VK_D) && x < 436) {
-            xMove = movespeed;
-        }
-    }
+		// Wird die Taste "A" gedrückt und die Position des Spielers ist innerhalb des Fensters...
+		if (game.getKeyManager().statusTasten.contains(KeyEvent.VK_A) && x > 1) {
+			xMove = -movespeed;								// ... bewege den Spieler nach links.
 
-    @Override
-    public void update() {
+		}
+		// Wird die Taste "D" gedrückt und die Position des Spielers ist innerhalb des Fensters...
+		if (game.getKeyManager().statusTasten.contains(KeyEvent.VK_D) && x < 436) {
+			xMove = movespeed;								// ... bewege den Spieler nach rechts.
+		}
+	}
 
-        getInput();
-        move();
-        shoot();
-    }
+	@Override
+	public void update() {
 
-    @Override
-    public void render(Graphics graphics) {
-        graphics.drawImage(Assets.player, (int) x, (int) y, Unit.STANDARD_UNIT_WIDTH, Unit.STANDARD_UNIT_HEIGHT, null);
+		getInput();
+		move();
+		shoot();
+	}
 
-    }
-    
+	@Override
+	public void render(Graphics graphics) {
+		graphics.drawImage(Assets.player, (int) x, (int) y, Unit.STANDARD_UNIT_WIDTH, Unit.STANDARD_UNIT_HEIGHT, null);
+
+	}
+
 }
